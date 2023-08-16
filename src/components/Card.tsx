494ded5cb71e51b3cf6ecf1a91e5209fc7090740
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-08-16 22:07:19 +0200

    Card Animation Update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,8 +4,8 @@ const Card = () => {
 	return (
 		<div className="card">
 			<div className="card__content">
-				<div className="card__side card__side--front">TEST FRONT</div>
-				<div className="card__side card__side--back">TEST BACK</div>
+				<div className="card__side card__side--front">Question</div>
+				<div className="card__side card__side--back">Answer</div>
 			</div>
 		</div>
 	);
