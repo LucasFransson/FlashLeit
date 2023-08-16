4d2f5ebe523f136143981ae279814c9e4cf3050c
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-08-16 19:34:04 +0200

    Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div className="card">
+			<div className="card__content">
+				<div className="card__side card__side--front">TEST FRONT</div>
+				<div className="card__side card__side--back">TEST BACK</div>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
