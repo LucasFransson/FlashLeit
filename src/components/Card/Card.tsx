faf822c7442d368ad57a2bd62cb24d3acfbee3d2
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-08-18 22:15:25 +0200

    Changed Card Flip Target

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+// import React[useState]from 'react';
+import { useState } from 'react';
 
 const Card = () => {
+	const [isFlipped, setIsFlipped] = useState(false);
+
+	const handleFlipClick = () => {
+		setIsFlipped((prevIsFlipped) => !prevIsFlipped);
+	};
+
 	return (
-		<div className="card">
+		<div className={`card ${isFlipped ? 'is-flipped' : ''}`}>
 			<div className="card__content">
 				<div className="card__side card__side--front">
-					<h1 className="card__heading">Question</h1>
-					<p className="card__text">
-						This is a question bla bla bla bla bla bla bla
-					</p>
+					<h1 className="card__heading">Q</h1>
+					<div className="card__center">
+						<p className="card__text">
+							What framework is C# closely associated with?
+						</p>
+						<button className="flip-button" onClick={handleFlipClick}>
+							Flip Card
+						</button>
+					</div>
 				</div>
 				<div className="card__side card__side--back">
-					<h1 className="card__heading">Answer</h1>
+					<h1 className="card__heading">A</h1>
 					<p className="card__text">
-						This is an answer bla bla bla bla bla bla bla
+						C# is closely associated with the .NET framework.
 					</p>
 				</div>
 			</div>
