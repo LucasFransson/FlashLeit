8b565a352203d7def0f49fddf7abdc980c4ab0ec
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-08-23 00:40:16 +0200

    Implemented RoutesConfig file + set up Router in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 //import React, { ComponentType, Suspense, useState, lazy } from 'react'; //TODO: Remove, only for testing loading state
 import React, { Suspense, useState, lazy } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import RoutesConfig from './utils/RoutesConfig';
 import LoadingIcon from './components/LoadingIcon';
 
 import './App.css';
 import '../public/css/style.css';
-import HomePage from './pages/HomePage';
 
+function App() {
+	return (
+		<Router>
+			<div className="App">
+				<Suspense fallback={<LoadingIcon />}>
+					<RoutesConfig />
+					{/* //TODO: Remove, only for testing loading state */}
+					{/* <LazyLoadedComponent /> */}
+				</Suspense>
+			</div>
+		</Router>
+	);
+}
+
+// LAZY LOADED COMPONENT FOR TESTING
 // const LazyLoadedComponent: ComponentType = lazy(
 // 	//TODO: Remove, only for testing loading state
 // 	() =>
@@ -17,20 +33,5 @@ import HomePage from './pages/HomePage';
 // 			}, 10000)
 // 		)
 // );
-
-function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<div className="App">
-			<Suspense fallback={<LoadingIcon />}>
-				<HomePage></HomePage>
-				{/* <Card></Card> */}
-				{/* //TODO: Remove, only for testing loading state */}
-				{/* <LazyLoadedComponent /> */}
-			</Suspense>
-		</div>
-	);
-}
 
 export default App;
