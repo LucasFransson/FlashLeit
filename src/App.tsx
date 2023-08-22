c28fd6b23d4430356b507f84b09f98a3fb6dea37
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-08-22 23:04:27 +0200

    Suspense Loading Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import Card from './components/Card/Card';
+// import React, { ComponentType, Suspense, useState, lazy } from 'react'; //TODO: Remove, only for testing loading state
+import React, { Suspense, useState, lazy } from 'react';
+import LoadingIcon from './components/LoadingIcon';
+
 import './App.css';
 import '../public/css/style.css';
 import HomePage from './pages/HomePage';
+
+// const LazyLoadedComponent: ComponentType = lazy(
+// 	//TODO: Remove, only for testing loading state
+// 	() =>
+// 		new Promise<any>((resolve) =>
+// 			setTimeout(() => {
+// 				import('./components/LazyLoadedComponent').then((module) =>
+// 					resolve({ default: module.default })
+// 				);
+// 			}, 3000)
+// 		)
+// );
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
-			<HomePage></HomePage>
-			{/* <Card></Card> */}
-		</>
+		<div className="App">
+			<Suspense fallback={<LoadingIcon />}>
+				<HomePage></HomePage>
+				{/* <Card></Card> */}
+				{/* //TODO: Remove, only for testing loading state */}
+				{/* <LazyLoadedComponent /> */}
+			</Suspense>
+		</div>
 	);
 }
 
---
+++ src/components/LoadingIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LoadingIcon() {
+	return <div className="loading">Loading...</div>;
+}
+
+export default LoadingIcon;
