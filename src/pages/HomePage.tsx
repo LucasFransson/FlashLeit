2e2bbc1d8020fb0ffc934f02b71893ee3a30efa6
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-08-28 10:05:27 +0200

    User auth/Login

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,33 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import {
+	AuthenticatedTemplate,
+	UnauthenticatedTemplate,
+	useMsal,
+} from '@azure/msal-react';
+
 function HomePage() {
+	// TODO: Refactor login/logout to login button component
+	const { instance } = useMsal();
+	const [idToken, setIdToken] = useState('');
+
+	const Login = async () => {
+		try {
+			let { idToken } = await instance.loginPopup();
+			setIdToken(idToken);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	const Logout = async () => {
+		try {
+			await instance.logoutPopup();
+			setIdToken('');
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<>
 			<div className="home-page">
@@ -9,6 +37,18 @@ function HomePage() {
 					Aside<Link to={'/about'}>About</Link>
 					<Link to={'/cardset'}>Cards</Link>
 					<Link to={'/edit'}>Edit</Link>
+					<AuthenticatedTemplate>
+						<p>You are authenticated</p>
+						<button type="button" onClick={() => Logout()}>
+							Logout
+						</button>
+					</AuthenticatedTemplate>
+					<UnauthenticatedTemplate>
+						<p>You are not authenticated!</p>
+						<button type="button" onClick={() => Login()}>
+							Login
+						</button>
+					</UnauthenticatedTemplate>
 				</aside>
 			</div>
 		</>
