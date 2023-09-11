c864f9c7929894919c8b56cb2dbed81f985917e3
Daniel Svensson <danielsvenssonn@gmail.com>
2023-09-11 16:18:54 +0200

    Added cache to usersSlice

--- src/redux/api/usersSlice.ts
+++ src/redux/api/usersSlice.ts
@@ -4,9 +4,11 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
 	endpoints: builder => ({
 		getAllUsers: builder.query({
 			query: () => "api/users",
+			providesTags: ["Users"],
 		}),
 		getUserById: builder.query({
 			query: id => `api/users/${id}`,
+			providesTags: (result, error, id) => [{ type: "Users", id: id }],
 		}),
 		updateUser: builder.mutation({
 			query: ({ id, ...user }) => ({
@@ -14,12 +16,14 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
 				method: "PATCH",
 				body: user,
 			}),
+			invalidatesTags: (result, error, id) => [{ type: "Users", id: id }],
 		}),
 		deleteUser: builder.mutation({
 			query: ({ id, objectId }) => ({
 				url: `api/users/${id}/${objectId}`,
 				method: "DELETE",
 			}),
+			invalidatesTags: (result, error, id) => [{ type: "Users", id: id }],
 		}),
 	}),
 });
