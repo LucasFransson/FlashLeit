a9474aad1332870abd8c4ea1b6946cb877b4ee91
Daniel Svensson <danielsvenssonn@gmail.com>
2023-09-13 20:25:20 +0200

    Added idTokenSlice to store.js, added code in apiSlice to use idToken as auth header.

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,8 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
 export const apiSlice = createApi({
 	reducerPath: "api",
-	baseQuery: fetchBaseQuery({ baseUrl: "https://flashleit.azure-api.net/" }),
+	baseQuery: fetchBaseQuery({
+		baseUrl: "https://flashleit.azure-api.net/",
+		credentials: "include",
+		prepareHeaders: (headers, { getState }) => {
+			const idToken = (getState() as RootState).idToken;
+			if (idToken) {
+				headers.set("authorization", `Bearer ${idToken}`);
+			}
+
+			return headers;
+		},
+	}),
 	tagTypes: ["Collection", "Users", "Achievements", "UserCollections"],
 	endpoints: builder => ({}),
 });
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import userIdReducer from "./userIdSlice";
+import idTokenReducer from "./idTokenSlice";
 
 export const store = configureStore({
 	reducer: {
-		// [usersApi.reducerPath]: usersApi.reducer,
-		// [collectionsApi.reducerPath]: collectionsApi.reducer,
 		[apiSlice.reducerPath]: apiSlice.reducer,
 		userId: userIdReducer,
+		idToken: idTokenReducer,
 	},
 	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
 });
