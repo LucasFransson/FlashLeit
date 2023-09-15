b12d90deedd74693be9c56c2d5b88e9587677de9
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-09-15 10:22:26 +0200

    LeitnerBox Hook

--- src/components/LeitnerBoxes/LeitnerBoxes.tsx
+++ src/components/LeitnerBoxes/LeitnerBoxes.tsx
@@ -6,63 +6,71 @@ import LeitnerBoxesTypes from '../../types/LeitnerBoxesTypes';
 import { determineBoxToStudy } from '../../utils/determineLeitnerBox';
 import CardTypes from '../../types/CardTypes';
 import { use } from 'chai';
+import useLeitnerBox from '../../hooks/useLeitnerBox';
 
 // extend necessary props here
 // interface LeitnerBoxes extends CardCollectionTypes {}
 
 function LeitnerBoxes({ collection, selectBox }) {
-	const [leitnerBox1, setLeitnerBox1] = useState<CardTypes[]>([]);
-	const [leitnerBox2, setLeitnerBox2] = useState<CardTypes[]>([]);
-	const [leitnerBox3, setLeitnerBox3] = useState<CardTypes[]>([]);
-
-	const [amountOfCardsToPlayBox1, setAmountOfCardsToPlayBox1] = useState(0);
-	const [amountOfCardsToPlayBox2, setAmountOfCardsToPlayBox2] = useState(0);
-	const [amountOfCardsToPlayBox3, setAmountOfCardsToPlayBox3] = useState(0);
-
-	const [isLeitnerBox1Empty, setIsLeitnerBox1Empty] = useState(true);
-	const [isLeitnerBox2Empty, setIsLeitnerBox2Empty] = useState(true);
-	const [isLeitnerBox3Empty, setIsLeitnerBox3Empty] = useState(true);
-
-	// Date variables:
-
-	const currentDate = new Date();
-
 	const oneDay = 24 * 60 * 60 * 1000;
 	const threeDays = 72 * 60 * 60 * 1000;
 	const sevenDays = 168 * 60 * 60 * 1000;
 
-	useEffect(() => {
-		const box1Cards = collection.flashCards.filter(
-			(card) =>
-				card.leitnerIndex === 1 &&
-				currentDate.getTime() - card.lastReviewedDate.getTime() >= oneDay
-		);
-		const box2Cards = collection.flashCards.filter(
-			(card) =>
-				card.leitnerIndex === 2 &&
-				currentDate.getTime() - card.lastReviewedDate.getTime() >= threeDays
-		);
-		const box3Cards = collection.flashCards.filter(
-			(card) =>
-				card.leitnerIndex === 3 &&
-				currentDate.getTime() - card.lastReviewedDate.getTime() >= sevenDays
-		);
+	const box1 = useLeitnerBox(collection.flashCards, 1, oneDay);
+	const box2 = useLeitnerBox(collection.flashCards, 2, threeDays);
+	const box3 = useLeitnerBox(collection.flashCards, 3, sevenDays);
 
-		setLeitnerBox1(box1Cards);
-		setLeitnerBox2(box2Cards);
-		setLeitnerBox3(box3Cards);
-
-		setAmountOfCardsToPlayBox1(box1Cards.length);
-		setAmountOfCardsToPlayBox2(box2Cards.length);
-		setAmountOfCardsToPlayBox3(box3Cards.length);
-	}, [collection]);
-
-	const selectLeitnerBox = (leitnerBox: CardTypes[]) => {
+	const selectLeitnerBox = (leitnerBox) => {
 		selectBox(leitnerBox);
 	};
 
-	console.log(leitnerBox1, leitnerBox2, leitnerBox3);
-	console.log(collection);
+	// const [leitnerBox1, setLeitnerBox1] = useState<CardTypes[]>([]);
+	// const [leitnerBox2, setLeitnerBox2] = useState<CardTypes[]>([]);
+	// const [leitnerBox3, setLeitnerBox3] = useState<CardTypes[]>([]);
+
+	// const [amountOfCardsToPlayBox1, setAmountOfCardsToPlayBox1] = useState(0);
+	// const [amountOfCardsToPlayBox2, setAmountOfCardsToPlayBox2] = useState(0);
+	// const [amountOfCardsToPlayBox3, setAmountOfCardsToPlayBox3] = useState(0);
+
+	// const [isLeitnerBox1Empty, setIsLeitnerBox1Empty] = useState(true);
+	// const [isLeitnerBox2Empty, setIsLeitnerBox2Empty] = useState(true);
+	// const [isLeitnerBox3Empty, setIsLeitnerBox3Empty] = useState(true);
+
+	// Date variables:
+
+	// const currentDate = new Date();
+
+	// useEffect(() => {
+	// 	const box1Cards = collection.flashCards.filter(
+	// 		(card) =>
+	// 			card.leitnerIndex === 1 &&
+	// 			currentDate.getTime() - card.lastReviewedDate.getTime() >= oneDay
+	// 	);
+	// 	const box2Cards = collection.flashCards.filter(
+	// 		(card) =>
+	// 			card.leitnerIndex === 2 &&
+	// 			currentDate.getTime() - card.lastReviewedDate.getTime() >= threeDays
+	// 	);
+	// 	const box3Cards = collection.flashCards.filter(
+	// 		(card) =>
+	// 			card.leitnerIndex === 3 &&
+	// 			currentDate.getTime() - card.lastReviewedDate.getTime() >= sevenDays
+	// 	);
+
+	// 	// setLeitnerBox1(box1Cards);
+	// 	// setLeitnerBox2(box2Cards);
+	// 	// setLeitnerBox3(box3Cards);
+
+	// 	// setAmountOfCardsToPlayBox1(box1Cards.length);
+	// 	// setAmountOfCardsToPlayBox2(box2Cards.length);
+	// 	// setAmountOfCardsToPlayBox3(box3Cards.length);
+
+	// }, [collection]);
+
+	// const selectLeitnerBox = (leitnerBox: CardTypes[]) => {
+	// 	selectBox(leitnerBox);
+	// };
+
 	return (
 		<div className="leitnerbox__wrapper">
 			<div className="leitnerbox__top">
@@ -71,29 +79,29 @@ function LeitnerBoxes({ collection, selectBox }) {
 			<div
 				className="leitnerbox__1"
 				onClick={() => {
-					selectLeitnerBox(leitnerBox1);
+					selectLeitnerBox(box1.box);
 				}}
 			>
 				<h2>Box 1</h2>
-				<h3>Cards: {amountOfCardsToPlayBox1}</h3>
+				<h3>Cards: {box1.playableCardCount}</h3>
 			</div>
 			<div
 				className="leitnerbox__2"
 				onClick={() => {
-					selectLeitnerBox(leitnerBox2);
+					selectLeitnerBox(box2.box);
 				}}
 			>
 				<h2>Box 2</h2>
-				<h3>Cards: {amountOfCardsToPlayBox2}</h3>
+				<h3>Cards: {box2.playableCardCount}</h3>
 			</div>
 			<div
 				className="leitnerbox__3"
 				onClick={() => {
-					selectLeitnerBox(leitnerBox3);
+					selectLeitnerBox(box3.box);
 				}}
 			>
 				<h2>Box 3</h2>
-				<h3>Cards: {amountOfCardsToPlayBox3}</h3>
+				<h3>Cards: {box3.playableCardCount}</h3>
 			</div>
 		</div>
 	);
